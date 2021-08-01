3a10d968fbd0a16c10c0be872c7c0bc3790e2e55
Luc Tielen <luc.tielen@kabisa.nl>
2021-08-01 21:30:28 +0200

    Parse facts

--- grammar.js
+++ grammar.js
@@ -1,6 +1,19 @@
 module.exports = grammar({
   name: "eclair",
   rules: {
-    source_file: $ => "hello"
+    source_file: $ => repeat($._statement),
+    _statement: $ => $.fact,
+    fact: $ => seq(
+      $.identifier,
+      '(', $.argument_list, ')',
+      '.'
+    ),
+    argument_list: $ => seq(
+      $._argument,
+      repeat(seq(',', $._argument))
+    ),
+    _argument: $ => choice($.identifier, $.lit),
+    identifier: _ => /[a-zA-Z][a-zA-Z0-9_]*/,
+    lit: _ => /\d+/
   }
 });
